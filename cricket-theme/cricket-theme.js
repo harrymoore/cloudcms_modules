94c22955d854ef1e9e908364b6773e06bff18805
Harry Moore <harry.moore@cloudcms.com>
2018-02-15 16:18:07 -0500

    fix theme module path

--- cricket-theme/cricket-theme.js
+++ cricket-theme/cricket-theme.js
@@ -1,10 +1,12 @@
 define(function(require, exports, module) {
 
+    var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
+
     // require("css!bootstrap/../../css/bootstrap.css");
     // require("css!app/../../main.css");
 
     // require("css!themes/theme-reset.css");
     // require("css!./theme.css");
 
-    require("css!./cricket-theme.css");
+    require("css!" + moduleId + "/cricket/cricket-theme.css");
 });
